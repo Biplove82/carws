34d3e099600e02de216353436444c9ac79ab7adc
Biplove <93914620+Biplove82@users.noreply.github.com>
2023-11-30 22:26:07 +0530

    reset auth

--- src/Controller/AuthController.js
+++ src/Controller/AuthController.js
@@ -5,46 +5,7 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const JWT_SECRET = "your-secret-key";
 
-// //user registeration
-// const userregistertration = async function (req, res) {
-//   try {
-//     const {
-//       userName,
-//       passWord,
-//       firstName,
-//       surName,
-//       role,
-//       mobileNumber,
-//       alternateNumber,
-//       address,
-//     } = req.body;
-//     const existinguser = await authmodels.findOne({ userName: userName });
-//     if (existinguser) {
-//       return res.status(409).send("username already exists");
-//     }
-//     let hasedpassword = await bcrypt.hash(passWord, 10);
-
-//     const newuser = new authmodels({
-//       userName: userName,
-//       passWord: hasedpassword,
-//       firstName: firstName,
-//       surName: surName,
-//       role,
-//       mobileNumber: mobileNumber,
-//       alternateNumber: alternateNumber,
-//       address: address,
-//     });
-//     await newuser.save();
-//     res
-//       .status(200)
-//       .json({ id: newuser._id, msg: "User Registered Succesfully" });
-//   } catch (error) {
-//     res.status(500).json({ message: "Server Error" + error });
-//   }
-// };
-
 const userRegister = async function (req, res) {
-
   try {
     const {
       userName,
@@ -55,7 +16,6 @@ const userRegister = async function (req, res) {
       mobileNumber,
       alternateNumber,
       address,
-      
     } = req.body;
     const existingUser = await authmodels.findOne({ userName: userName });
     if (existingUser) {
@@ -71,7 +31,6 @@ const userRegister = async function (req, res) {
       mobileNumber,
       alternateNumber,
       address,
-      
     });
     await newUser.save();
     res
@@ -87,143 +46,149 @@ const userRegister = async function (req, res) {
 const sendotp = async function (req, res) {
   try {
     const { userName } = req.body;
-
-    // Find the user by email
-    // const user = await authmodels.findOne({ userName });
-
-    // // Check if the user exists
-    // if (!user) {
-    //   return res
-    //     .status(400)
-    //     .json({ success: false, message: "User not found" });
-    // }
-
-    // Generate a random 6-digit OTP
+  
     const otp = Math.floor(100000 + Math.random() * 900000);
 
-    // Save the new OTP to the user in the database
-    // user.otp = otp;
-    // await otp.save();
-
-    // Send the OTP to the user's email
     var transport = nodemailer.createTransport({
-      host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
+      service: "gmail",
       auth: {
-        user: "5baa42b75630c2",
-        pass: "7a0468f22aaaeb",
+        user: "biplavmandal.mandal@gmail.com",
+        pass: "abnj vnct roto spfn",
       },
     });
     const info = await transport.sendMail({
-      from: '"Your Name" <biplovmandal.mandla@gmail.com>', // Update with your name and email
+      from: '"Your Name" <biplavmandal.mandal@gmail.com>', // Update with your name and email
       to: userName,
       subject: "Email Verification OTP",
       text: `Your OTP for email verification is: ${otp}`,
       html: `<b>Your OTP for email verification is: ${otp}</b>`,
     });
+    transport.sendMail(info, (err, result) => {
+      if (err) {
+        console.log("Error");
+      }
 
-    res
-      .status(200)
-      .json({
+      res.status(200).json({
         message: "OTP sent successfully. Check your email for OTP.",
         otp: otp,
       });
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
-//forgetpasswprd 
-const forgetpasswprd= async function(req,res){
+
+
+//forgetpasswprd
+const forgetpasswprd = async function (req, res) {
   try {
     const { userName } = req.body;
     const otp = Math.floor(100000 + Math.random() * 900000);
-    const user = await authmodels.findOneAndUpdate({ userName }, { otp }, { new: true, upsert: true });
+    const user = await authmodels.findOneAndUpdate(
+      { userName },
+      { otp },
+      { new: true, upsert: true }
+    );
 
     var transport = nodemailer.createTransport({
-      host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
+      service: "gmail",
       auth: {
-        user: "5baa42b75630c2",
-        pass: "7a0468f22aaaeb",
+        user: "biplavmandal.mandal@gmail.com",
+        pass: "abnj vnct roto spfn",
       },
     });
     const info = await transport.sendMail({
-      from: '"Your Name" <biplovmandal.mandla@gmail.com>', // Update with your name and email
+      from: '"Your Name" <biplavmandal.mandal@gmail.com>', // Update with your name and email
       to: userName,
       subject: "Email Verification OTP",
       text: `Your OTP for email verification is: ${otp}`,
       html: `<b>Your OTP for email verification is: ${otp}</b>`,
     });
-    res.status(200).json({
-      message: 'OTP sent successfully. Check your email for OTP.',
-      userId: user._id,
-      userName:userName,
-      otp:otp, // You may want to include the user ID in the response for further verification steps
-    });
- } catch (error) {
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
-  }
-}
-//resetpassowrd
-const resetpass= async function(req,res){
-  try {
-    const { userName, otp, newPassword } = req.body;
-    const user = await authmodels.findOne({ userName:userName, otp });
-    if (!user) {
-      return res.status(400).json({ success: false, message: 'Invalid OTP' });
-    }
-    user.passWord = newPassword;
-    user.otp = null;
-   
-    await user.save();
-    // console.log(user);
-    res.status(200).json({ success: true, message: 'Password reset successful' });
-  
-  } catch (error) {
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
-    
-  }
-}
-//resend otp
-const resendOtp =async function(req,res){
-  try {
-    const {userName}=req.body;
-    const user = await authmodels.findOne({ userName });
-      // Check if the user exists
-      if (!user) {
-        return res.status(400).json({ success: false, message: 'User not found' });
+    transport.sendMail(info, (err, result) => {
+      if (err) {
+        console.log("Error");
       }
-      let otp = Math.floor(100000 + Math.random() * 900000);
-      user.otp = otp;
-      await user.save();
-      var transport = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-          user: "5baa42b75630c2",
-          pass: "7a0468f22aaaeb",
-        },
-      });
-      const info = await transport.sendMail({
-        from: '"Your Name" <biplovmandal.mandla@gmail.com>', // Update with your name and email
-        to: userName,
-        subject: "Email Verification OTP",
-        text: `Your OTP for email verification is: ${otp}`,
-        html: `<b>Your OTP for email verification is: ${otp}</b>`,
-      });
       res.status(200).json({
-        message: 'OTP resent successfully. Check your email for OTP.',
+        message: "OTP sent successfully. Check your email for OTP.",
         otp: otp,
+        userName: userName,
+        userId: user._id,
       });
-} catch (error) {
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" + error });
   }
 };
 
-    
-  
 
+//resetpassowrd
+const resetpass = async function (req, res) {
+  try {
+    const { userName, otp, newPassword } = req.body;
+    const user = await authmodels.findOne({ userName: userName, otp });
+    if (!user) {
+      return res.status(400).json({ success: false, message: "Invalid OTP" });
+    }
+    user.passWord = newPassword;
+    user.otp = null;
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: "Password reset successful" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+//resend otp
+const resendOtp = async function (req, res) {
+  try {
+    const { userName } = req.body;
+    const user = await authmodels.findOne({ userName });
+    // Check if the user exists
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+    let otp = Math.floor(100000 + Math.random() * 900000);
+    user.otp = otp;
+     await user.save();
+    // console.log(x);
+    var transport = nodemailer.createTransport({
+      service: "gmail",
+      // host: "sandbox.smtp.mailtrap.io",
+      // port: 2525,
+     auth: {
+        user: "biplavmandal.mandal@gmail.com", //"5baa42b75630c2",
+        pass: "abnj vnct roto spfn", //"7a0468f22aaaeb",
+      },
+    });
+    const info = await transport.sendMail({
+      from: '"Your Name" <biplavmandal.mandal@gmail.com>', // Update with your name and email
+      to: userName,
+      subject: "Email Verification OTP",
+      text: `Your OTP for email verification is: ${otp}`,
+      html: `<b>Your OTP for email verification is: ${otp}</b>`,
+    });
+    transport.sendMail(info, (err, result) => {
+      if (err) {
+        console.log("Error is sending Mail", err);
+      }
+      res.status(200).json({
+        message: "OTP resent successfully. Check your email for OTP.",
+        otp: otp,
+      });
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
 //user registration with mobile number
 
 const resbymobnum = async function (req, res) {
@@ -263,5 +228,12 @@ const login = async function (req, res) {
   }
 };
 
-
-module.exports = { userRegister,resbymobnum, login, sendotp,forgetpasswprd,resetpass,resendOtp};
+module.exports = {
+  userRegister,
+  resbymobnum,
+  login,
+  sendotp,
+  forgetpasswprd,
+  resetpass,
+  resendOtp,
+};
